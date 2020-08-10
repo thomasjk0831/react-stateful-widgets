844c70289d0595726ad6897cd337c40622d657b4
Thomas Kim <Thomasjk0831@gmail.com>
2020-08-10 19:09:36 -0400

    finished todo list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,39 +1,43 @@
 import React, { useState } from 'react'
 
-const list = [{id: 0, todo:"Wake up"}, {id:1, todo:"Eat breakfast"}, {id:2, todo:"Shower"}, 
-{id:3, todo:"Attend lecture"}, {id: 4, todo:"Work on Project"}
+const list = [{id: 0, todo:"Wake up", done: false}, {id:1, todo:"Eat breakfast", done: false}, {id:2, todo:"Shower", done: false}, 
+{id:3, todo:"Attend lecture",  done: false}, {id: 4, todo:"Work on Project", done: false}
 ]
 
 function Todos(){
 
+    
+
     const [listTodos, updateList] = useState(list)
-    const [activeTodo, updateTodo] = useState(true)
+    
+    function handleClick(id){
+        let newArr = [...listTodos]
+        newArr[id].done = !newArr[id].done
+        updateList(newArr)
+     }
 
-    const styleTodo = {
-        text-decoration : "line-through"
+     console.log(listTodos)
+      return(
+          <div className ="container">
+             {
+                 
+                     
+                     listTodos.map((item) =>
+                       <div key={item.id} style = {item.done? {textDecoration: 'line-through'} : {textDecoration: 'none'}} >
+                         <button onClick={()=> handleClick(item.id)}>X</button>{item.todo} 
+                       </div>
+                     )
+                   
+             }
+         </div>
+           
+      )
+   
     }
+    
+    export default Todos
 
-    function clickHandler(){
-       console.log("clicked")
-    }
+       
 
-    console.log(listTodos)
-     return(
-         <div className ="container">
-            {
-                
-                    
-                    listTodos.map((item) =>
-                      <div key={item.id} style = {styleTodo} onClick = {clickHandler}>
-                        <button>X</button>{item.todo} 
-                      </div>
-                    )
-                  
-            }
-        </div>
-          
-     )
+
      
-}
-
-export default Todos
